90fc4407422963c9f7cf90335cc0340103d8c8fc
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-02-28 15:11:04 +0000

    edit the admin site views for the Category and Account models, as well as set up media files config

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    # sets the user data to be shown in the list view
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active',)
+    # allows the following fields to act as links to the instance
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -127,3 +127,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+
+# media files configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
