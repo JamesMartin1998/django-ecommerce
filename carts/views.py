5574a451fc435d14d1a607363ad9fac1bd6b3ec6
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-03-02 13:32:29 +0000

    add cart edit and deletes functionality

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -47,11 +47,62 @@ def add_cart(request, product_id):
             quantity=1
         )
         cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()
+
+    return redirect('cart')
+
+
+def remove_cart(request, product_id):
+    """
+    decrement quantity of cart items by one using the - button
+    """
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    # when a cart item's quantity is one, we can just delete it 
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
     return redirect('cart')
 
 
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, cart_items=None, total=0, quantity=0):
+    """
+    Get all cart items in a cart, as well as the total price and quanity of items in the cart
+    """
+    try:
+        # get the cart using the session id as cart id
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        # get the cart items in the cart
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+
+        # loop through each cart item and edit the total cart price and quanity
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    
+    # if no cart or cart items
+    except ObjectDoesNotExist:
+        pass # don't do anything
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
+    }
+
+    return render(request, 'store/cart.html', context)
