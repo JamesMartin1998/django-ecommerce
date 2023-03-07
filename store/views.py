4a559b7777391873d9957accdcde559805e05e2d
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-03-07 11:54:58 +0000

    edit cart links and add context processor to display the number of items in the cart

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
 
 # Create your views here.
 
@@ -32,12 +34,16 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         # access then category model via FK, then slug field
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug )
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        # get the cart items in the user's cart, then returns True or False if items exist
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart
     }
 
 
